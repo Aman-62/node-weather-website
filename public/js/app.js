56d961c3f0c86bd324e3a80287a4d734b917dd1d
Amandeep Singh <aman230630@gmail.com>
2021-12-20 17:07:04 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side javascript file is loaded');
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msgOne = document.querySelector('#msg-1')
@@ -7,23 +5,23 @@ const msgTwo = document.querySelector('#msg-2')
 
 
 weatherForm.addEventListener('submit', (e) => {
- e.preventDefault()
- msgTwo.textContent = ""
- const location = search.value
- console.log(location);
- if (!location) {
-  msgOne.textContent = 'please enter location'
-  return console.log('please enter location');
- }
- const url = '/weather?address=' + location
- fetch(url).then((response) => {
-  response.json().then((data) => {
-   if (data.error) {
-    return msgOne.textContent = data.error
-   }
-   msgOne.textContent = data.location
-   msgTwo.textContent = data.forecast
-   console.log(data);
+  e.preventDefault()
+  msgTwo.textContent = ""
+  const location = search.value
+  console.log(location);
+  if (!location) {
+    msgOne.textContent = 'please enter location'
+    return console.log('please enter location');
+  }
+  const url = '/weather?address=' + location
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        return msgOne.textContent = data.error
+      }
+      msgOne.textContent = data.location
+      msgTwo.textContent = data.forecast
+      console.log(data);
+    })
   })
- })
 })
